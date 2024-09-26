a03c185aa118f03218d873848de54a43b476ee43
angeleduardodm <angeleduardo200213@gmail.com>
2024-09-26 16:53:09 -0400

    first view done

--- vueApp/src/App.vue
+++ vueApp/src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
-    <header class="p-4 bg-blue-600 text-white">
-      <h1 class="text-3xl">¿Quieres ser un chef reconocido?</h1>
-    </header>
-    <section class="p-8">
-      <h2 class="text-2xl font-semibold mb-4">Lo que incluye el curso:</h2>
-      <ul class="list-disc pl-6 space-y-2 text-lg">
-        <li>12 módulos con 2 temas cada uno</li>
-        <li>Material en PDF</li>
-        <li>24 podcasts de las clases teóricas</li>
-        <li>Live todos los sábados</li>
+  <!-- <nav class="fixed top-0 left-0 right-0 w-full z-50">
+    <div class="container mx-auto flex justify-end">
+      <ul class="menu menu-horizontal flex bg-orange-900 space-x-8">
+        <li><a href="#home" class="text-white hover:text-gray-400">Home</a></li>
+        <li><a href="#about" class="text-white hover:text-gray-400">About</a></li>
+        <li><a href="#services" class="text-white hover:text-gray-400">Services</a></li>
+        <li><a href="#contact" class="text-white hover:text-gray-400">Contact</a></li>
       </ul>
+    </div>
+  </nav> -->
+  <div class="min-h-screen bg-gray-100 flex w-full">
+    <div class="lg:w-1/2 p-10 flex w-6">
+      <section class="p-8">
+        <article class="prose lg:prose-2xl">
+          <h1 class="text-secondary">¿QUIERES SER UN CHEF RECONOCIDO?</h1>
+          <h3 class="text-primary">¡Forma parte del nuevo grupo!</h3>
+          <p class=" text-secondary text-2xl font-semibold mb-4">Lo que incluye el curso:</p>
+          <ul class="text-secondary list-disc pl-6 space-y-2 text-lg">
+            <li>12 módulos con 2 temas cada uno</li>
+            <li>Material en PDF</li>
+            <li>24 podcasts de las clases teóricas</li>
+            <li>Live todos los sábados</li>
+          </ul>
+        </article>
+      </section>
 
-      <!-- Botón para inscribirse -->
-      <div class="mt-8">
-        <router-link to="/form" class="btn btn-primary">Inscribirme ahora</router-link>
+    <div class="lg:w-1/2 flex items-center p-8 m-10 ">
+      <div class="avatar">
+        <div class="w-80 rounded-full bg-accent">
+          <img src="./assets/bettina.png" />
+        </div>
       </div>
-    </section>
+    </div>
+    </div>
+
+          
+    <div class="flex justify-center mt-8 w-full">
+      <router-link to="/form" class="btn btn-secondary rounded-xl">Inscribirme ahora</router-link>
+    </div>
   </div>
 </template>
 
@@ -25,7 +46,3 @@ export default {
   name: 'App'
 }
 </script>
-<<<<<<< HEAD
-git merge master
-=======
->>>>>>> master
--- vueApp/tailwind.config.js
+++ vueApp/tailwind.config.js
@@ -4,8 +4,26 @@ module.exports = {
     "./index.html", 
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
+  themes: [
+
+    ],
+
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#452F27",
+          "secondary": "#7C411E",
+          "accent": "#FFE9D2",
+          "neutral": "#281b1e",
+          "base-100": "#fffbff",
+          "info": "#0093ce",
+          "success": "#00d442",
+          "warning": "#d48800",
+          "error": "#ff1c66",
+            },
+      },
+    ],
   },
-  plugins: [require('daisyui')],
 };
